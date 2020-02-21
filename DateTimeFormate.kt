1fba2cc4e6515c38b9a234e0e22fab462ebd0977
shehryarahmed <shehreyarahmed5@gmail.com>
2020-02-22 00:20:21 +0500

    Date time formate Sample

---
+++ DateTimeFormate.kt
@@ -0,0 +1,12 @@
+    val formateStyle0 = "dd-MM-yyyy";//21-02-2020
+    val formateStyle1 = "dd-MMMM-YYYY";//21-February-2020
+    val formateStyle11 = "dd-MMM-YYYY";//21-Feb-2020
+    val formateStyle2 = "EEEE-MMM-YYYY";//Friday-Feb-2020
+    val formateStyle3 = "EE-MMM-YYYY";//Fri-Feb-2020
+
+    val formateStyle4 = "hh:mm:ss"; //04:22:22
+    val formateStyle5 = "HH:MM:SS"; //16:02:25
+    val formateStyle6 = "hh:mm:ss a"; //04:41:09 PM
+    val formateStyle7 = "hh:mm:ss a z"; //04:43:56 PM GMT+05:00
+
+    val formateStyle8 = "dd-MMM-yyyy hh:mm:ss";//21-2-2020 04:22:22
